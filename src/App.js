768e44e65d94cea5fa5fdc649865c950afe73a99
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2025-11-14 14:59:36 +0100

    added role tooltip and fixed styles

--- src/App.js
+++ src/App.js
@@ -161,9 +161,9 @@ function App() {
           </div>
           <AccessibleCheckbox />
           <div className="tooltip-wrapper">
-              <p><span className="tooltip-container">
+              <p><span aria-describedby="tooltip-text" className="tooltip-container" tabIndex={0}>
                   Lorem Ipsum
-                  <span className="tooltip-text">Hello</span>
+                  <span id="tooltip-text" role="tooltip" className="tooltip-text">Hello</span>
               </span> is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                   when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
